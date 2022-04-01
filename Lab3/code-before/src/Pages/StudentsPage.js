650e5a4e3998bd69e974c986adaf0bb5240bd8cd
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 14:46:00 +0200

    Add buttons to handle search

--- Lab3/code-before/src/Pages/StudentsPage.js
+++ Lab3/code-before/src/Pages/StudentsPage.js
@@ -1,3 +1,4 @@
+import "./StudentsPage.css"
 import { useState, useEffect } from "react";
 import StudentService from "../Models/StudentService";
 import StudentEntry from "./Components/StudentEntry";
@@ -6,17 +7,29 @@ import StudentSearch from "./Components/StudentSearch";
 
 function StudentsPage() {
   const [students, setStudents] = useState([]);
+  const [searchButtonContent, setSearchButtonContent] = useState("Rozwiń wyszukiwanie");
+  const [showSearch, setShowSearch] = useState(false);
 
   useEffect(()=>{
     setStudents(StudentService.getStudents());
   }, [])
 
+  useEffect(()=>{
+      setSearchButtonContent(showSearch?"Zwiń wyszukiwanie":"Rozwiń wyszukiwanie");
+  }, [showSearch])
+
+  const handleShowSearchButton = () => {
+    setShowSearch(!showSearch);
+  }
+
   return (
     <div className="StudentsPage">
-        <StudentSearch></StudentSearch>
-        <button type="button" className="btn btn-primary">Dodaj nowe ogłoszenie!</button>
-        {students.map((student)=><StudentEntry student={student} key={"student_" + student.id}></StudentEntry>)
-        }
+      <div className="students-buttons">
+        <button type="button" className="btn btn-dark" onClick={handleShowSearchButton}>{searchButtonContent}</button>
+        <button type="button" className="btn btn-dark">Dodaj nowe ogłoszenie!</button>
+      </div>
+      {showSearch && <StudentSearch></StudentSearch>}
+      {students.map((student)=><StudentEntry student={student} key={"student_" + student.id}></StudentEntry>)}
     </div>
   );
 }
