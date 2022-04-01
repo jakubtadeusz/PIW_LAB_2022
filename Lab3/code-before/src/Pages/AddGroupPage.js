927b326e600a606eebe8ca18336535a5ea99a186
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 19:44:26 +0200

    Add pages to groups

--- Lab3/code-before/src/Pages/AddGroupPage.js
+++ Lab3/code-before/src/Pages/AddGroupPage.js
@@ -1,6 +1,31 @@
+import React from 'react';
 
-function AddGroupPage () {
-    return(<p>Add group</p>);
+
+class AddGroupPage extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            query: "abc"
+        }
+    }
+
+    updateQuery = (event) => {
+        this.setState({
+            query: event.target.value
+        });
+    }
+
+
+    render () {
+        return (
+            <div>
+                <input value={this.state.query} onChange={this.updateQuery}/>
+                Add Group
+            </div>
+        );
+    }
 }
+
 
 export default AddGroupPage;
--- Lab3/code-before/src/Pages/GroupsPage.js
+++ Lab3/code-before/src/Pages/GroupsPage.js
@@ -1,6 +1,46 @@
+import React from 'react';
+import GroupService from '../Models/GroupService'
 
-function GroupsPage () {
-    return (<p>Groups</p>);    
+class GroupsPage extends React.Component {
+    
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            groups: [],
+            searchButtonContent: "Rozwiń wyszukiwanie",
+            showSearch: false,
+            courseToFilter: "",
+            descriptionToFilter: ""
+        }
+
+        this.navigate = props.navigate;
+    }
+
+    componentDidMount(){
+        this.setState({groups: GroupService.getGroups()});
+    }
+  
+    handleShowSearchButton = () => {
+      this.setState({searchButtonContent: (!this.state.showSearch)?"Zwiń wyszukiwanie":"Rozwiń wyszukiwanie"});
+      this.setState({showSearch: !this.state.showSearch});
+    }
+
+    handleAddGroupButton = () =>{
+        this.navigate("/groups/add");
+    }
+
+    render () {
+        return (
+            <div className='GroupsPage'>
+                <div className="groups-buttons">
+                    <button type="button" className="btn btn-dark" onClick={this.handleShowSearchButton}>{this.state.searchButtonContent}</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleAddGroupButton}>Dodaj nową grupę!</button>
+                </div>
+                {this.state.groups.map(group=><p>{group.name}</p>)}
+            </div>
+        );
+    }
 }
 
 export default GroupsPage;
