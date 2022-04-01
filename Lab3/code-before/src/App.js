f7fd92c9a56afbc85cad3189452124aaef42286d
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 19:43:39 +0200

    Fix navigation

--- Lab3/code-before/src/App.js
+++ Lab3/code-before/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+import {Routes, Route, NavLink} from 'react-router-dom';
 import StudentsPage from './Pages/StudentsPage';
 import AddStudentPage from './Pages/AddStudentPage';
 import GroupsPage from './Pages/GroupsPage';
 import AddGroupPage from './Pages/AddGroupPage';
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
     <header>
@@ -13,22 +15,19 @@ function App() {
       <h3>Sometimes repo is better than tinder</h3>
     </header>
     <main>
-      <BrowserRouter>
       <nav>
         <NavLink to="/students">Studenci</NavLink>
         <NavLink to="/groups">Grupy projektowe</NavLink>
       </nav>
       <div className="AppContent">
-      <Routes>
-        <Route path="/" element={<StudentsPage/>}/>
-        <Route path="/students" element={<StudentsPage/>} />
-        <Route path='/students/add'element={<AddStudentPage/>}/>
-        <Route path="/groups" element={<GroupsPage/>} />
-        <Route path="/groups/add" element={<AddGroupPage/>} />
-
-      </Routes>
+        <Routes>
+          <Route path="/" element={<StudentsPage/>}/>
+          <Route path="/students" element={<StudentsPage/>} />
+          <Route path='/students/add'element={<AddStudentPage/>}/>
+          <Route path="/groups" element={<GroupsPage navigate={navigate}/>} />
+          <Route path="/groups/add" element={<AddGroupPage/>} />
+        </Routes>
       </div>
-      </BrowserRouter>
     </main>
     <footer>Jakub Tadeusz - 256760 - PIW 2022</footer>
 
