2fefe5963921d1ea06436c6b2598735fc3d70778
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 14:46:06 +0200

    Add search component

--- Lab3/code-before/src/Pages/Components/StudentSearch.js
+++ Lab3/code-before/src/Pages/Components/StudentSearch.js
@@ -1,7 +1,42 @@
+import "./StudentSearch.css"
+import {useState} from "react"
 
+function StudentSearch (props) {
 
-function StudentSearch(props) {
-    return (<h2>Search</h2>);
+    let [tags, setTags] = useState(["test", "test2"]);
+
+    const handleCourseSearchChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handleTagsSearchChange = (event) => {
+        const tags = event.target.value.split(",").map(tag=>tag.trim()).filter(tag=>tag!=="");
+        setTags(tags);
+    }
+
+    const handleDescSearchChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    return (
+    <div className="StudentSearch">
+        <div className="course-search">
+            <input type={"text"} placeholder="Szukaj po przedmiocie" className="form-control" onChange={(event) => handleCourseSearchChange(event)}></input>
+        </div>
+        <div className="tags-search">
+            <input type={"text"} placeholder="Szukaj po tagach, przecinek rozpoczyna nowy tag" className="form-control" onChange={(event) => handleTagsSearchChange(event)}></input>
+            {(tags!==undefined && tags.length > 0) &&
+                <div className="tags-preview">
+                    {tags.map((tag, id)=>{
+                        return <div className="tag" key={"search-tag_" + id}>{tag}</div>
+                    })}                    
+                </div>
+            }
+        </div>
+        <div className="description-search">
+            <textarea placeholder="Szukaj po opisie" className="form-control" rows="2" onChange={(event) => handleDescSearchChange(event)}></textarea>
+        </div>
+    </div>
+    );
 }
-
 export default StudentSearch;
