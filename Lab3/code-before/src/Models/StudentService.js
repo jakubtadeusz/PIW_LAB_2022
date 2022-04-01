a3f5e1fb5a34bbdeda4b4bc995b8275feb7b5d01
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 13:11:09 +0200

    Add description and date to the student objects

--- Lab3/code-before/src/Models/StudentService.js
+++ Lab3/code-before/src/Models/StudentService.js
@@ -8,14 +8,18 @@ const StudentService = {
                 surname: "Tadeusz",
                 email: "example@mail.com",
                 tags: ["React.js", "Angular", "C#", "ASP.NET", ".NET 5.0", "Node.js", "TypeScript", "frontend", "backend"],
-                courses: ["PIW", "AK2", "RiPO"]
+                courses: ["PIW", "AK2", "RiPO"],
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                date: new Date(2022, 3, 10, 15, 27, 32, 21)
             },{
                 id: 1,
                 name: "Jacek",
                 surname: "Trzeszczyński",
                 email: "example2@mail.com",
                 tags: ["Unity", "C#", "frontend"],
-                courses: ["PiPG"]
+                courses: ["PiPG"],
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                date: new Date(2022, 3, 5, 12, 37, 6, 33)
             },
             {
                 id: 2,
@@ -23,7 +27,9 @@ const StudentService = {
                 surname: "Konieczny",
                 email: "example3@mail.com",
                 tags: ["Java", "Spring", "backend", "Angular", "TypeScript", "frontend"],
-                courses: ["PZ", "UCiSW"]
+                courses: ["PZ", "UCiSW"],
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                date: new Date(2022, 2, 27, 4, 35, 10, 49)
             }
         ];
     }
