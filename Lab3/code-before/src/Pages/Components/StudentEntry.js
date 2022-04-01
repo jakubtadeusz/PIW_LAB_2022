9c9d37092d8558599222745af747a11595185aa1
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 13:35:50 +0200

    Format code in student entry component

--- Lab3/code-before/src/Pages/Components/StudentEntry.js
+++ Lab3/code-before/src/Pages/Components/StudentEntry.js
@@ -10,16 +10,23 @@ function StudentEntry(props) {
 
   return (
     <div className="StudentEntry">
-      {student.courses !== undefined && <div className="courses" key={student.id}>
-      {student.courses.map((course, i)=><div className="course" key={i}>{course}</div>)}
-      </div>}
+      {student.courses !== undefined && 
+        <div className="courses" key={student.id}>
+          {student.courses.map((course, i)=>{
+            <div className="course" key={i}>{course}</div>
+          })}
+        </div>
+      }
       <h4>{student.name} {student.surname}</h4>
       <p>{student.description}</p>
-        {student.tags !== undefined && <div className="tags" key={student.id}>
-        {student.tags.map((tag, i)=><div className="tag" key={i}>{tag}</div>)}
-      </div>}
+      {student.tags !== undefined && 
+        <div className="tags" key={student.id}>
+          {student.tags.map((tag, i)=>{
+            <div className="tag" key={i}>{tag}</div>
+          })}
+        </div>
+      }
     </div>
-    
   );
 }
 
