92404241ac4631b76cf4d4c99fd553ab1f0bd774
taydexx <jakub.tadeusz@interia.pl>
2022-04-01 13:29:46 +0200

    Update student service to handle entry additions

--- Lab3/code-before/src/Models/StudentService.js
+++ Lab3/code-before/src/Models/StudentService.js
@@ -1,37 +1,49 @@
 
+
+let newId = 0;
+
+const students = [
+    {
+        id: newId++,
+        name: "Jakub",
+        surname: "Tadeusz",
+        email: "example@mail.com",
+        tags: ["React.js", "Angular", "C#", "ASP.NET", ".NET 5.0", "Node.js", "TypeScript", "frontend", "backend"],
+        courses: ["PIW", "AK2", "RiPO"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        date: new Date(2022, 3, 10, 15, 27, 32, 21)
+    },{
+        id: newId++,
+        name: "Jacek",
+        surname: "Trzeszczyński",
+        email: "example2@mail.com",
+        tags: ["Unity", "C#", "frontend"],
+        courses: ["PiPG"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        date: new Date(2022, 3, 5, 12, 37, 6, 33)
+    },
+    {
+        id: newId++,
+        name: "Krzysztof",
+        surname: "Konieczny",
+        email: "example3@mail.com",
+        tags: ["Java", "Spring", "backend", "Angular", "TypeScript", "frontend"],
+        courses: ["PZ", "UCiSW"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        date: new Date(2022, 2, 27, 4, 35, 10, 49)
+    }
+];
+
 const StudentService = {
+
     getStudents: () => {
-        return [
-            {
-                id: 0,
-                name: "Jakub",
-                surname: "Tadeusz",
-                email: "example@mail.com",
-                tags: ["React.js", "Angular", "C#", "ASP.NET", ".NET 5.0", "Node.js", "TypeScript", "frontend", "backend"],
-                courses: ["PIW", "AK2", "RiPO"],
-                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                date: new Date(2022, 3, 10, 15, 27, 32, 21)
-            },{
-                id: 1,
-                name: "Jacek",
-                surname: "Trzeszczyński",
-                email: "example2@mail.com",
-                tags: ["Unity", "C#", "frontend"],
-                courses: ["PiPG"],
-                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                date: new Date(2022, 3, 5, 12, 37, 6, 33)
-            },
-            {
-                id: 2,
-                name: "Krzysztof",
-                surname: "Konieczny",
-                email: "example3@mail.com",
-                tags: ["Java", "Spring", "backend", "Angular", "TypeScript", "frontend"],
-                courses: ["PZ", "UCiSW"],
-                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                date: new Date(2022, 2, 27, 4, 35, 10, 49)
-            }
-        ];
+        return students;
+    },
+
+    addStudent: (student) => {
+        student.id = newId++;
+        students.push(student);
+        return student.id;
     }
 }
 
